a6f744dbbd6bba82f4b04e94ce0f6f82ce4c317d
Candid <olacandid@gmail.com>
2024-02-14 15:59:52 +0100

    'updated'

--- function.js
+++ function.js
@@ -1,3 +1,69 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAmR8uGzpUAPYfhbhz9lCPjYiwOiXr2VOQ",
+    authDomain: "myportfoliodb-d466d.firebaseapp.com",
+    databaseURL: "https://myportfoliodb-d466d-default-rtdb.firebaseio.com",
+    projectId: "myportfoliodb-d466d",
+    storageBucket: "myportfoliodb-d466d.appspot.com",
+    messagingSenderId: "312458689917",
+    appId: "1:312458689917:web:69a8ef0e43f5f73ce16014"
+  };
+
+    // Initialize Firebase
+    const app = firebase.initializeApp(firebaseConfig);
+
+    var contactMeDB = firebase.database().ref('ContactMe');
+
+    document.getElementById('ContactMe').addEventListener('submit', submitform);
+
+    function submitform(e) {
+        e.preventDefault();
+        
+        let name = document.getElementById('name').value;
+        let email = document.getElementById('email').value;
+        let subject = document.getElementById('subject').value;
+        let message = document.getElementById('message').value;
+
+        saveMessages(name, email, subject, message);
+
+        //enable pre-loader
+        document.querySelector('.preloader').style.display = "block";
+
+        setTimeout(() => {
+        document.querySelector('.preloader').style.display = "none";
+            
+        }, 4000);
+
+        document.querySelector('.displayResponse').style.display = "block"
+
+
+        setTimeout(() => {
+        document.querySelector('.displayResponse').style.display = "none"
+            
+        }, 3000);
+
+
+        //reset form
+        document.getElementById('ContactMe').reset();
+
+        // console.log(name,email,subject,message);
+    }
+
+    const saveMessages = (name, email, subject, message) =>{
+        var newPorfolioForm = contactMeDB.push();
+
+        newPorfolioForm.set({
+            name : name,
+            email : email,
+            subject : subject,
+            message : message
+
+        });
+    }
+
+
+
+
+
 $(document).ready(function(){
     $(window).scroll(function(){
         if(this.scrollY > 20){
